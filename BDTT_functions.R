db33328bcd1976ac97f08fd334ebdd2274b0b90a
FloMazel <flo.mazel@gmail.com>
2019-04-05 17:20:51 -0700

    Update using the castor R package

--- BDTT_functions.R
+++ BDTT_functions.R
@@ -1,50 +1,53 @@
-
-BDTT=function(similarity_slices,tree,sampleOTUs,onlyBeta=T,metric=c("jac","bc")){
+New_toOld_OTUs_castor=function(similarity,tree)
+{
+  Ntips=length(tree$tip.label)
+  NameTips=tree$tip.label
   
-  Betas=lapply(similarity_slices,getBDTT,tree=tree,sampleOTUs=sampleOTUs,onlyBeta=onlyBeta,metric=metric)
-  names(Betas)=similarity_slices
-  res=do.call(function(...){abind(...,along=0)},Betas)
-  return(res)
+  #Collapse tips accorindg to the resolution 
+  ColapsedNodes=collapse_tree_at_resolution(tree,similarity)
+  DescendingTips=lapply(ColapsedNodes$collapsed_nodes,tree,FUN=function(i,tree){get_subtree_at_node(tree, i)$subtree$tip.label})
+  names(DescendingTips)=as.character(ColapsedNodes$collapsed_nodes+Ntips)
+  
+  
+  #Lenght of the different categories of OTUs
+  N_newOTUS=length(DescendingTips)
+  N_collapsedOldOtus= sum(do.call(rbind, lapply(DescendingTips, function(x) length(x))))
+  N_totalNewOTUS=Ntips-N_collapsedOldOtus+N_newOTUS
+  
+  print(paste(similarity," similarity provides ",N_totalNewOTUS," total new OTUs",sep=""))
+  
+  NewOTUs_OldOTUs_Matrix=NA
+  if (N_totalNewOTUS>2) 
+  {
+    #Names of the different categories of OTUs
+    collapsedOldOtus= unlist(DescendingTips)
+    UncollapsedOTUs=NameTips[!NameTips%in%collapsedOldOtus]
+    
+    #Creating New to Old matrix correpondance
+    NewOTUSNames=c(UncollapsedOTUs,names(DescendingTips))
+    NewOTUs_OldOTUs_Matrix=Matrix(0,ncol=N_totalNewOTUS,nrow=Ntips,dimnames = list(tree$tip.label,NewOTUSNames))
+    
+    #Filling  New to Old matrix correpondance / uncollapsed tips
+    if (length(UncollapsedOTUs)>1){diag(NewOTUs_OldOTUs_Matrix[UncollapsedOTUs,UncollapsedOTUs])=1}     #just fill the diagonal as tips are unchanged
+    if (length(UncollapsedOTUs)==1){NewOTUs_OldOTUs_Matrix[UncollapsedOTUs,UncollapsedOTUs]=1}
+    
+    #Filling  New to Old matrix correpondance / collapsed tips
+    for (i in names(DescendingTips)){NewOTUs_OldOTUs_Matrix[DescendingTips[[i]],i]=1}
+    
+  } else{print("Only 2 OTUS (or less) present at this resolution -- are you sure this is meaningfull?")} 
+  
+  
+  return(NewOTUs_OldOTUs_Matrix)
 }
 
-
-
-#The first function is copied from phytools
-getDescendants=function (tree, node, curr = NULL) 
+getNew_OTUs_sample_matrix=function(similarity,sampleOTUs,tree)
 {
-  #if (!inherits(tree, "phylo")) 
-   # stop("tree should be an object of class \\"phylo\\".")
-  if (is.null(curr)) 
-    curr <- vector()
-  daughters <- tree$edge[which(tree$edge[, 1] == node), 2]
-  curr <- c(curr, daughters)
-  if (length(curr) == 0 && node <= length(tree$tip.label)) 
-    curr <- node
-  w <- which(daughters > length(tree$tip.label))
-  if (length(w) > 0) 
-    for (i in 1:length(w)) curr <- getDescendants(tree, daughters[w[i]], 
-                                                  curr)
-  return(curr)
-}
-
-
-
-
-getBDTT=function(similarity,tree,sampleOTUs,onlyBeta=T,metric="jac")
-{
-  New_OTUs_sample_matrix=getNew_OTUs_sample_matrix(similarity=similarity,sampleOTUs=sampleOTUs,tree=tree)
-  BetasPA=getBeta(t(New_OTUs_sample_matrix[[1]]),ab=F)[c("jtu","jac"),,]
-  #rownames(BetasPA)=paste(rownames(BetasPA),"_Simi_",similarity,sep="")
-  BetasAb=getBeta(t(New_OTUs_sample_matrix[[1]]),ab=T)[c("bctu","bc"),,]  
-  #rownames(BetasAb)=paste(rownames(BetasAb),"_Simi_",similarity,sep="")
-  AllBetas=abind(BetasPA,BetasAb,along = 1)
-  
-  AllBetas=AllBetas[metric,,]
-  
-  if (onlyBeta==F){res=list(Beta_Div=AllBetas,NewOTU_table=New_OTUs_sample_matrix[[1]],New_to_old_OTUs=New_OTUs_sample_matrix[[2]])}
-  if (onlyBeta==T){res=AllBetas}
-  
-  return(res)
+  CorresMatrix=New_toOld_OTUs_castor(similarity=similarity,tree=tree)
+  sample=Matrix(t(sampleOTUs))
+  sp=colnames(sample)
+  Newsample=t(sample[,sp] %*% CorresMatrix[sp,])
+  out=list(NewSample=as.matrix(Newsample),CorrepondanceOTUs=CorresMatrix)
+  return(out)
 }
 
 getBeta=function(mat,ab=F)
@@ -63,96 +66,62 @@ getBeta=function(mat,ab=F)
     mat[mat>0]=1
     h=beta.pair(data.frame(mat), index.family="jaccard")
     hh=beta.pair(data.frame(mat), index.family="sorensen")
-    jtu=as.matrix(h[[1]])
-    jne=as.matrix(h[[2]])
-    jac=as.matrix(h[[3]])
-    stu=as.matrix(hh[[1]])
-    sne=as.matrix(hh[[2]])
-    sor=as.matrix(hh[[3]])  
+    jtu=as.matrix(h[["beta.jtu"]])
+    jne=as.matrix(h[["beta.jne"]])
+    jac=as.matrix(h[["beta.jac"]])
+    stu=as.matrix(hh[["beta.sim"]])
+    sne=as.matrix(hh[["beta.sne"]])
+    sor=as.matrix(hh[["beta.sor"]])  
     res=abind(jtu,jne,jac,stu,sne,sor,along=0)
     dimnames(res)[[1]]=c("jtu","jne","jac","stu","sne","sor")  
   }
   return(res)
 }
 
+#getBDTT=function(similarity,tree,sampleOTUs,onlyBeta=T,metric="jac")
+#{
+#  New_OTUs_sample_matrix=getNew_OTUs_sample_matrix(similarity=similarity,sampleOTUs=sampleOTUs,tree=tree)
+  
+  
+#  BetasPA=getBeta(t(New_OTUs_sample_matrix[[1]]),ab=F)[c("jtu","jac"),,]
+  #rownames(BetasPA)=paste(rownames(BetasPA),"_Simi_",similarity,sep="")
+#  BetasAb=getBeta(t(New_OTUs_sample_matrix[[1]]),ab=T)[c("bctu","bc"),,]  
+  #rownames(BetasAb)=paste(rownames(BetasAb),"_Simi_",similarity,sep="")
+#  AllBetas=abind(BetasPA,BetasAb,along = 1)
+  
+#  AllBetas=AllBetas[metric,,]
+  
+#  if (onlyBeta==F){res=list(Beta_Div=AllBetas,NewOTU_table=New_OTUs_sample_matrix[[1]],New_to_old_OTUs=New_OTUs_sample_matrix[[2]])}
+#  if (onlyBeta==T){res=AllBetas}
+  
+#  return(res)
+#}
 
-getNew_OTUs_sample_matrix=function(similarity,sampleOTUs,tree)
+
+getBDTT=function(similarity,tree,sampleOTUs,onlyBeta=T)
 {
-  CorresMatrix=New_toOld_OTUs(similarity=similarity,tree=tree)
-  sample=Matrix(t(sampleOTUs))
-  sp=colnames(sample)
-  Newsample=t(sample[,sp] %*% CorresMatrix[sp,])
-  out=list(NewSample=as.matrix(Newsample),CorrepondanceOTUs=as.matrix(CorresMatrix))
-  return(out)
+  New_OTUs_sample_matrix=getNew_OTUs_sample_matrix(similarity=similarity,sampleOTUs=sampleOTUs,tree=tree)
+  OTU_table=t(New_OTUs_sample_matrix[[1]])
+  
+  Bray=as.matrix(vegdist(OTU_table,method="bray"))
+  Jac=as.matrix(vegdist(OTU_table,method="jac",binary = T))
+  JacTT=as.matrix(designdist(OTU_table, "(2*pmin(b,c))/(a+2*pmin(b,c))",abcd=TRUE,terms = "binary"))
+
+  AllBetas=abind(Bray,Jac,JacTT,along = 0)
+  dimnames(AllBetas)[[1]]=c("Bray","Jac","Jac_TT")
+  
+  if (onlyBeta==F){res=list(Beta_Div=AllBetas,NewOTU_table=New_OTUs_sample_matrix[[1]],New_to_old_OTUs=New_OTUs_sample_matrix[[2]])}
+  if (onlyBeta==T){res=AllBetas}
+  
+  return(res)
 }
 
-getHnode=function(node,tree)
-{ 
-  tips=1:length(tree$tip.label)
-  NH=node.depth.edgelength(tree)
-  DescNodes=getDescendants(node=node,tree=tree)
-  DescTips=DescNodes[DescNodes%in%tips]
-  Hnode=max(NH[DescTips]-NH[node])# take the longest branches to define node height
-  return(Hnode)
+BDTT=function(similarity_slices,tree,sampleOTUs,onlyBeta=T){
+  
+  Betas=lapply(similarity_slices,getBDTT,tree=tree,sampleOTUs=sampleOTUs,onlyBeta=onlyBeta)
+  names(Betas)=similarity_slices
+  res=do.call(function(...){abind(...,along=0)},Betas)
+  return(res)
 }
-
-getHnodes=function(tree)
-{
-  allnodes=sort(unique(c(tree$edge)))
-  names(allnodes)=as.character(allnodes)
-  tips=1:length(tree$tip.label)
-  nodes=allnodes[!allnodes%in%tips]
-  
-  Hnodes=sapply(nodes,getHnode,tree=tree)
-  return(Hnodes)
-}
-
-multigetDescendants=function(node,tree){
-  alltips=1:length(tree$tip.label)
-  nodes=getDescendants(tree=tree,node=node)
-  tips=nodes[nodes%in%alltips]
-  return(tree$tip.label[tips])
-}
-
-New_toOld_OTUs=function(similarity,tree)
-{
-  Ntips=length(tree$tip.label)
-  tips=1:Ntips
-  NameTips=tree$tip.label
-  Hnodes=getHnodes(tree) #get Nodes height 
-  Hbranches=cbind(Hnodes[as.character(tree$edge[,1])],Hnodes[as.character(tree$edge[,2])]) #put them in a matrix of edges
-  NodeToCluster=tree$edge[(Hbranches[,1]>similarity)&(Hbranches[,2]<=similarity),2] #select the branches whom descandant node to be collapsed
-  NodeToCluster=NodeToCluster[!is.na(NodeToCluster)] #remove tips
-  
-  DescendandTips=lapply(NodeToCluster,multigetDescendants,tree=tree) #get descendant tips
-  names(DescendandTips)=as.character(NodeToCluster)
-  
-  #Lenght of the different categories of OTUs
-  N_newOTUS=length(DescendandTips)
-  N_collapsedOldOtus= sum(do.call(rbind, lapply(DescendandTips, function(x) length(x))))
-  N_totalNewOTUS=Ntips-N_collapsedOldOtus+N_newOTUS
-  
-  print(paste(similarity," similarity provides ",N_totalNewOTUS," total new OTUs",sep=""))
-  
-  NewOTUs_OldOTUs_Matrix=NA
-  if (N_totalNewOTUS>2) 
-  {
-    #Names of the different categories of OTUs
-    collapsedOldOtus= unlist(DescendandTips)
-    UncollapsedOTUs=NameTips[!NameTips%in%collapsedOldOtus]
-    
-    #Creating and filling New to Old matrix correpondance
-    NewOTUSNames=c(UncollapsedOTUs,as.character(NodeToCluster))
-    NewOTUs_OldOTUs_Matrix=Matrix(0,ncol=N_totalNewOTUS,nrow=Ntips,dimnames = list(tree$tip.label,NewOTUSNames))
-    NewOTUs_OldOTUs_Matrix=Matrix(0,ncol=N_totalNewOTUS,nrow=Ntips,dimnames = list(tree$tip.label,NewOTUSNames))
-    
-    for (i in UncollapsedOTUs){NewOTUs_OldOTUs_Matrix[i,i]=1} # uncollapsed OTUS
-    for (i in as.character(NodeToCluster)){NewOTUs_OldOTUs_Matrix[DescendandTips[[i]],i]=1} #collapsed OTUS
-  } else{print("Only 2 OTUS (or less) present at this resolution -- are you sure this is meaningfull?")} 
-  
-  
-  return(NewOTUs_OldOTUs_Matrix)
-}
-
 
 
